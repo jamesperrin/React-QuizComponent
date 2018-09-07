66de2182347eabb0e853f016945fb0771b5e3a02
James Perrin <countrydawgg@gmail.com>
2018-09-07 14:14:24 -0700

    Attempt 3 to fix issue

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class QuizQuestionButton extends Component { 
+    render() { 
+        const { button_text } = this.props;
+        
+        return (
+            <li>
+                <button>{button_text}</button>
+            </li>
+        )
+    }
+}
+
+export default QuizQuestionButton
